d3ab21a5414f0a6b120bd18af956bd01d0a68521
Josh <josh.r.hawk@gmail.com>
2010-05-14 14:34:58 -0400

    not sure why it didnt commit the first time

--- ruby_euler_solutions.rb
+++ ruby_euler_solutions.rb
@@ -13,6 +13,17 @@ def is_prime?(n)
   return true
 end
 
+def trial_prime_factors(n)
+  factors = []
+  (2..n).each do |i|
+    while n % i == 0 
+      factors << i 
+      n /= i
+    end
+  end
+  factors
+end
+
 ########################## SOLUTIONS
 
 def problem_1(sum = 0)
@@ -60,8 +71,16 @@ def problem_4(start = 999)
   end
 end
 
-def problem_5
-  # TODO
+# What is the smallest number divisible by each of the numbers 1 to 20?
+def problem_5(n=20)
+  factor_h = {} # prime => max number of occurrences 
+  (2..n).each do |i| 
+    h = {}
+    trial_prime_factors(i).each {|f| h[f] = h[f] + 1 rescue 1} 
+    h.each {|k,v| factor_h[k] = h[k] unless factor_h[k] && factor_h[k] > h[k]}
+  end
+  answer = factor_h.inject(1) {|a,h| a * h.first**h.last}
+  puts "problem_5 => #{answer}"
 end
 
 def problem_6(last = 100)
@@ -200,6 +219,60 @@ def problem_16
   a = a.to_s.split(//)
   puts "problem_16 => #{a.inject(0) {|sum,n| sum + n.to_i}}"
 end
+
+# How many letters would be needed to write all the numbers in words from 1 to 1000?
+=begin
+def problem_17
+  numbers_letter_count_h = {
+    0 => 0, 
+    1 => 3,    # one
+    2 => 3,    # two
+    3 => 5,    # three
+    4 => 4,    # four
+    5 => 4,    # five
+    6 => 3,    # six
+    7 => 5,    # seven
+    8 => 5,    # eight
+    9 => 4,    # nine
+
+    10 => 3,   # ten
+    11 => 6,   # eleven
+    12 => 6,   # twelve
+    13 => 8,   # thirteen
+    14 => 8,   # fourteen
+    15 => 7,   # fifteen
+    16 => 7,   # sixteen
+    17 => 9,   # seventeen
+    18 => 8,   # eighteen
+    19 => 8,   # nineteen
+
+    20 => 6,   # twenty
+    30 => 6,   # thirty
+    40 => 5,   # forty
+    50 => 5,   # fifty
+    60 => 5,   # sixty
+    70 => 7,   # seventy
+    80 => 6,   # eighty
+    90 => 6,   # ninety
+    100 => 7,  # hundred 
+    1000 => 8, # thousand
+    "and" => 3 # dumb!!!!!
+  }
+  sum = 0
+  def accum(n)
+    numbers_letter_count_h[n] ||
+    
+    
+    div = 10**(n.to_s.size - 1) 
+      numbers_letter_count_h[n] = 
+  end
+
+  (1..1000).each do |n|
+
+
+  end
+end
+=end
 
 ########################## DATA
 
