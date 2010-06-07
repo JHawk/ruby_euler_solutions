bf747bf9e423a9a239fb5c0f95e3499e583b2949
Josh <josh.r.hawk@gmail.com>
2010-06-07 08:47:13 -0400

    fixed 5

--- lib/problem05.rb
+++ lib/problem05.rb
@@ -1,18 +1,11 @@
 require File.dirname(__FILE__) + '/problem.rb'
-require File.dirname(__FILE__) + '/utils.rb'
 # What is the smallest number divisible by each of the numbers 1 to 20?
 class Problem05 < Problem 
-  include Utils
+  require 'rational'  
   
   def initialize; super end
   
   def problem(n=20)
-    factor_h = {} # prime => max number of occurrences 
-    (2..n).each do |i| 
-      h = {}
-      trial_prime_factors(i).each {|f| h[f] = h[f] + 1 rescue 1} 
-      h.each {|k,v| factor_h[k] = h[k] unless factor_h[k] && factor_h[k] > h[k]}
-    end
-    factor_h.inject(1) {|a,h| a * h.first**h.last}
+    (1..n).inject(1) {|p,i| p.lcm i}
   end
 end
