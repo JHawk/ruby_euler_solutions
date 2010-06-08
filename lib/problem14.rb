41e6e4302aa5173240f74be5bccd94eb5fbe656a
Josh <josh.r.hawk@gmail.com>
2010-06-08 12:06:02 -0400

    added skip problems

---
+++ lib/problem14.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/problem.rb'
+# Find the longest sequence using a starting number under one million.
+class Problem14 < Problem 
+  def initialize; super end
+
+  def problem
+    ""
+  end
+end
---
+++ lib/problem15.rb
@@ -0,0 +1,9 @@
+require File.dirname(__FILE__) + '/problem.rb'
+# Starting in the top left corner in a 20 by 20 grid, how many routes are there to the bottom right corner?
+class Problem15 < Problem 
+  def initialize; super end
+
+  def problem
+    ""
+  end
+end
