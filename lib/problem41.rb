1fd6e19cd40c5f41579bcaa4f5eeb14e22641966
Josh <josh.r.hawk@gmail.com>
2010-06-29 14:38:10 -0400

    41 was fun!

--- lib/problem41.rb
+++ lib/problem41.rb
@@ -5,11 +5,16 @@ require File.dirname(__FILE__) + '/problem.rb'
 class Problem41 < Problem
   def initialize ; super end
 
-  def problem
-    a = []
-    [9,8,7,6,5,4,3,2,1].each_permutation do |x|
-      a << x
+  def problem(a=[9,8,7,6,5,4,3,2,1])
+    ps = []
+    n = a.first
+    a.each_permutation {|x| ps << x}
+    a.each_with_index do |n,idx|
+      return nil if n <= 1
+      ps[0..n.factorial-1].each do |x| 
+        t = x[idx..x.size]
+        return t if t.to_i.prime? 
+      end
     end
-    a
   end
 end
