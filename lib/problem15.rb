f17ea9128e4aa5627fc4de900937013667f5dd4a
Josh <josh.r.hawk@gmail.com>
2010-06-30 13:51:31 -0400

    good ol math

--- lib/problem15.rb
+++ lib/problem15.rb
@@ -3,7 +3,10 @@ require File.dirname(__FILE__) + '/problem.rb'
 class Problem15 < Problem 
   def initialize; super end
 
-  def problem
-    ""
+  def problem(a=[20,20])
+    equiv_orderings_r = (a.[0]).factorial
+    equiv_orderings_d = a.[0] == a.[1] ? equiv_orderings_r : (a.[1]).factorial
+    total_moves = a.sum.factorial
+    total_moves / (equiv_orderings_r * equiv_orderings_d)
   end
 end
