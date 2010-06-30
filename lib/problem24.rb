19a2294ddffc2038ffe2726236791bcc2a63f0f4
Josh <josh.r.hawk@gmail.com>
2010-06-30 15:35:37 -0400

    cheap one liner useing my initializer perm - however, quite slow

---
+++ lib/problem24.rb
@@ -0,0 +1,8 @@
+require File.dirname(__FILE__) + '/problem.rb'
+
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+class Problem24 < Problem
+  def initialize; super end
+  
+  def problem(a=[0,1,2,3,4,5,6,7,8,9],n=1000000) a.permutations[n-1] end
+end
