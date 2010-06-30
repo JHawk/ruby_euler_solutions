6e3d9775bd97bb1dbb2e865ed91fa3c88fd30418
Josh <josh.r.hawk@gmail.com>
2010-06-30 10:02:56 -0400

    ew - could have probably done better

--- lib/problem79.rb
+++ lib/problem79.rb
@@ -4,26 +4,40 @@ require File.dirname(__FILE__) + '/problem.rb'
 
 class Problem79 < Problem
   def initialize 
-    @data = File.read("#{File.dirname(__FILE__)}/../data/passcode79.txt").split("\n").sort
+    @data = File.read("#{File.dirname(__FILE__)}/../data/passcode79.txt").split("\n")
     super 
   end
 
+  # TODO could track the head of the list to avoid searching for a key that 
+  # doesn't exist in values at end
   def problem(data=@data)
-  puts data
-  
-    t = {}
-    data.map.each do |c| 
-      tmp = {c[1] => [[c[0]],[c[2]]]}
-      t.merge!(tmp) do |k,ov,nv| 
-      
-      puts k
-      puts ov
-      puts nv
-      
-        [ov.first + nv.first,
-         ov.last + nv.last]
+    linked_list = {}
+    data.each do |c| 
+      code = c.split(//)
+      code.each_with_index do |digit,idx|
+        current = linked_list[digit]
+        next_digit = code[idx+1]
+        if current && current != next_digit
+          next_val = linked_list[next_digit]
+          while next_val
+            if next_val == current  
+              linked_list[digit] = next_digit
+              break
+            end
+            next_val = linked_list[next_val]
+          end
+        else
+          linked_list[digit] = next_digit
+        end
       end
     end
-    t
+    vals = linked_list.values
+    num_s = ""
+    head = linked_list.select {|k,_| !vals.include?(k)}.keys.first
+    while head
+      num_s << head
+      head = linked_list[head]
+    end
+    num_s
   end
 end
