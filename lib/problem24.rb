dfd24d1434bdc461a15a31b6e2a8dd748c6ed5b8
Josh <josh.r.hawk@gmail.com>
2010-06-30 15:39:54 -0400

    cheap one liner - had spelling errors

--- lib/problem24.rb
+++ lib/problem24.rb
@@ -5,5 +5,5 @@ class Problem24 < Problem
   def initialize; super end
   
   # TODO make this use each permutation or make a detect permutation that short circuits 
-  def problem(a=[0,1,2,3,4,5,6,7,8,9],n=1000000) a.permutations[n-1] end
+  def problem(a=[0,1,2,3,4,5,6,7,8,9], n=1000000) a.permutations[n-1] end
 end
