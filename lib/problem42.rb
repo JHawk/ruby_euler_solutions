b0a386393a3653e8b5923de09249e68d355fc273
Josh <josh.r.hawk@gmail.com>
2010-07-09 08:58:16 -0400

    max triangle number was too low

--- lib/problem42.rb
+++ lib/problem42.rb
@@ -2,30 +2,20 @@ require File.dirname(__FILE__) + '/utils.rb'
 
 # How many triangle words does the list of common English words contain?
 class Problem42 < Problem 
-  
   def initialize
     @data_42 = File.read("#{File.dirname(__FILE__)}/../data/words42.txt").split(",")
-    @triangle_words = {}
-    @letters = ("A".."Z").to_a
+    @alpha = [""] + ("A".."Z").to_a
     
     # should start using lazy sequences 
-    @triangle_numbers = Utils::triangle_numbers(@data_42.map(&:size).max)
+    @tri_nums = Utils::triangle_numbers(@data_42.map(&:size).max * @alpha.size)
     super
   end
   
-  def score(word) word.inject(0) {|s,l| s + @letters.index(l)} end
+  def score(word) 
+    word.gsub(/"/,"").gsub(/\s/,"").split(//).inject(0) {|s,l| s + @alpha.index(l)} 
+  end
   
   def problem(data=@data_42)
-    data.inject(0) do |sum,word|
-      key = word.gsub(/"/,"").gsub(/\s/,"").split(//).sort
-      value = @triangle_words[key]
-      if value.nil? 
-        (@triangle_words[key] = @triangle_numbers.include?(score(key))) ? sum + 1 : sum
-      elsif value
-        sum + 1
-      else
-        sum
-      end
-    end
+    data.inject(0) {|sum,word| sum += @tri_nums.include?(score(word)) ? 1 : 0}
   end
 end
