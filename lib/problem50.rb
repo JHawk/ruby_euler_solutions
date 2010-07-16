46977dbddbc8f8387595ac9acd11a9c66f17b3af
Josh <josh.r.hawk@gmail.com>
2010-07-16 11:29:54 -0400

    slightly faster like this

--- lib/problem50.rb
+++ lib/problem50.rb
@@ -8,7 +8,7 @@ class Problem50 < Problem
     loop do 
       (0..n).each do |idx|
         num = primes[idx..primes.size-1-(n-idx)].sum
-        return num if num.prime? && num < 1000000
+        return num if num < 1000000 && num.prime? 
       end
       n += 1
     end
