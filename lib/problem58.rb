88dd61fb30ad42d7834f6ee7c855278d7fc6b0db
Josh <josh.r.hawk@gmail.com>
2010-07-16 12:27:21 -0400

    much much much better - didnt understand the problem

--- lib/problem58.rb
+++ lib/problem58.rb
@@ -5,20 +5,15 @@ class Problem58 < Problem
   def problem(dec=0.1)
     count = 0.0 
     side = 3
-    last = side**2
-    d = side - 1
-    corners = [last, last - d, last - d*2, last - d*3]
     diagonal = 5.0
-    Utils::euler_sieve(700000000) do |prime|
-      if prime > last
-        return side if count / diagonal < dec
-        side += 2
-        last = side**2 
-        d = side - 1
-        corners = [last, last - d, last - d*2, last - d*3]
-        diagonal += 4
-      end
-      count += 1 if corners.include?(prime)  
+    loop do 
+      last = side**2
+      d = side - 1
+      corners = [last, last - d, last - d*2, last - d*3]
+      corners.each {|c| count += 1 if c.prime?}
+      return side if count / diagonal < dec
+      side += 2
+      diagonal += 4
     end
   end  
 end
